20ec22d573c63259a4cc4229e146a25e5c0287b2
Tushar Ranjan <thrrnjn1103@gmail.com>
2024-09-28 14:46:11 +0530

    added css for cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -13,39 +13,44 @@ function Cart() {
   }, [cart])
 
   return (
-    <div>
+    <div className='max-w-[1200px] flex flex-col mx-auto md:flex-row justify-center'>
       {
         cart.length === 0? 
         (
-          <div>
-            <h2>Cart is empty</h2>
+          <div className='flex flex-col h-[80vh] justify-center items-center gap-y-5'>
+            <h2 className='text-xl font-bold mb-4 font-mono'> Your Cart is Empty!</h2>
             <NavLink to='/'>
-              <button>Shop Now</button>
+              <button className='py-3 px-12 font-bold rounded-lg text-xl bg-green-600 uppercase text-white hover:bg-zinc-100 hover:text-green-600 transition duration-300 border-2 border-green-600'>Shop Now</button>
             </NavLink>
           </div>
         ): 
         (
-          <div>
-            <div>
+          <div className='flex md:flex-row flex-col'>
+            <div className='w-[100%] md:w-[60%] flex flex-col p-2' >
               {
                 cart.map((item) => (
                   <CartItem key={item.id} data={item}/>
                 ))
               }
             </div>
-            <div>
+            
+            <div className='flex flex-col p-5 gap-5 mt-5 md:w-[40%] w-[100%] h-[98%] justify-between'>
 
-              <div>
-                <div>Your Cart</div>
-                <div>Summary</div>
-                <p>
-                  <span>Total Items : {cart.length}</span>
+              <div className='flex flex-col gap-5'>
+                <div className='font-semibold text-xl uppercase text-green-800'>Your Cart</div>
+                <div className='text-5xl uppercase font-bold text-green-700  -mt-5'>Summary</div>
+                <p className='text-xl'>
+                  <span className='text-gray-700 font-semibold text-xl'>Total Items : {cart.length}</span>
                 </p>
               </div>
 
-              <div>
-                <p>Total Amount : INR {totalAmount}</p>
-                <button>Checkout</button>
+              <div className='flex flex-col'>
+                <p className='text-xl font-bold'>
+                  <span className='text-gray-700 font-semibold'>Total Amount : ₹ {totalAmount}</span>
+                </p>
+                <button className='bg-green-700 hover:bg-purple-50 rounded-lg text-white transition duration-200 ease-linear mt-5 border-2 border-green-600 font-bold hover:text-green-700 p-3 text-xl'>
+                  Checkout
+                </button>
               </div>
             </div>
           </div>
